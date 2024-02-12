3933d3389bda07acdc1ab1505d2b2ffee122b613
daniel <carlos.dlimacontato@gmail.com>
2024-02-12 12:23:08 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main (String[] args) {
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+	}
+}
+
